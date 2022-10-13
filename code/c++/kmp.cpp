2c9ffe3988eea5cadbda66fe5241c1fdb170c384
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-10-13 09:08:47 -0300

    Capítulo sobre processamento de strings

---
+++ code/c++/kmp.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class KMP {
+  string p;
+  vector<int> v;
+  int m;
+
+public:
+  KMP(const char *_p) : p(_p) {
+    m = p.size();
+    v.assign(m + 1, -1);
+    for (int i = 0, j = -1; i < m;) {
+      while (j >= 0 && p[i] != p[j])
+        j = v[j];
+      v[++i] = ++j;
+    }
+  }
+
+  vector<int> match(const char* t) {
+    vector<int> ans;
+
+    for (int i = 0, j = 0, n = strlen(t); i < n;) {
+      while (j >= 0 && t[i] != p[j])
+        j = v[j];
+      i++;
+      j++;
+      if (j == m) {
+        ans.push_back(i - j);
+        j = v[j];
+      }
+    }
+
+    return ans;
+  }
+};
